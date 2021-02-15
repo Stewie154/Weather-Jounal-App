a886774f6576f38384b47baa4a4f37551fbb524b
Stewie154 <69080603+Stewie154@users.noreply.github.com>
2021-02-15 15:44:45 +0000

    getData function (api call)
    Here I've written the getData function which makes a call to the openweathermap api.
    
    This is an async function that waits on the result of the api request. If the data is successfully retrieved, it is logged to the console and returnedin the try() area. If not the error is caught in the catch() area.
    
    This function is triggered by an click event on the generate button. It is working as of this commit, with weather data successfully being logged to the console from the api.
    
    In the next commit I'll continue to work on the chained promise which will store this data in the web app.

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
+//used to hide api key in .env file
+require('dotenv').config();
+
 // Require Express to run server and routes
 const express = require('express');
+
 // Start up an instance of app
 const app = express();
+
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 const bodyParser = require('body-parser');
@@ -16,27 +21,31 @@ const cors = require('cors');
 const { request } = require('http');
 const { response } = require('express');
 app.use(cors());
+
 // Initialize the main project folder
 app.use(express.static('website'));
-
-//GET route that returns projectData in server code
-app.get('/', sendData);
 
 //sendData callback function
 const sendData = (request, response) => {
     response.send(projectData);
 }
 
-//POST route that adds incoming data to projectData
-app.post('/', addData);
+//GET route that returns projectData in server code
+app.get('/', sendData);
 
 //addData callback function
 const addData = (request, response) => {
-    projectData.push(request.body.temperature);
-    projectData.push(request.body.date);
-    projectData.push(request.body.userResponse);
+    let newEntry = {
+        temperature: request.body.temperature,
+        date: request.body.date,
+        userResponse: request.body.userResponse
+    }
+    projectData.push(newEntry);
     console.log(projectData);
 }
+
+//POST route that adds incoming data to projectData
+app.post('/', addData);
 
 // Setup Server
 const port = 8000;
@@ -46,3 +55,5 @@ const listening = () => {
 }
 
 const server = app.listen(port, listening);
+
+console.log(process.env);
--- website/app.js
+++ website/app.js
@@ -1,5 +1,59 @@
 /* Global Variables */
+const apiKey = 'c385c18d161b14df158500c5452637e5';
+const countryCode = 'US';
+let userZipCode;
+let baseUrl = `http://api.openweathermap.org/data/2.5/weather?zip=${userZipCode},${countryCode}&appid=${apiKey}`;
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+//grabs generate button, when it's clicked run sendData function with relevant parameters for api call
+document.getElementById('generate').addEventListener('click', function(){
+    userZipCode = document.getElementById('zip').value;
+    baseUrl = `http://api.openweathermap.org/data/2.5/weather?zip=${userZipCode},${countryCode}&appid=${apiKey}`
+    getData(baseUrl)
+    // .then(function(data){
+    //     console.log(data);
+    //     //add data to post request
+    //     let userResponse = document.getElementById('feelings').value;
+    //     postData('/addData', {temperature: data.main.temp, date: newDate, userResponse: userResponse})
+    // })
+});
+
+//getData async function to make get request to OpenWeatherMap api (get the weather)
+const getData = async(url) => {
+    //await response of call to api for data
+    const response = await fetch(url);
+    //if fetch call successful, run try code
+    try {
+        //wait for data to be fetched and turned in json
+        const data = await response.json();
+        //see the json data in console
+        console.log(data);
+        return data;
+    }
+    //if fetch call unsuccessful, run catch code
+    catch (error){
+        console.log('Error!', error);
+    }
+}
+
+//postData async function to store the weather data in the app 
+const postData = async(path, dataObject) => {
+    const response = await fetch(path, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dataObject),
+    });
+    try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+    } catch (error) {
+        console.log('Error!', error);
+    }
+}
